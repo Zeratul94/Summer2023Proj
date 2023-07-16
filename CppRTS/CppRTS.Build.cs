41c6e6bdbd80239cf2399561491cd46c3f9c2f09
Gedalya <ghmrgordon@gmail.com>
2023-07-15 23:14:17 -0400

    UE sync folder

---
+++ CppRTS/CppRTS.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class CppRTS : ModuleRules
+{
+	public CppRTS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+    }
+}
--- Source/CppRTS.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CppRTS.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CppRTS, "CppRTS" );
-
-DEFINE_LOG_CATEGORY(LogCppRTS)
- 
